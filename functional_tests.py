76a70149448acf6a5cf2429ce2e17870a07a8fa8
Eron Lloyd <eron@mawcom.com>
2017-08-12 17:01:08 -0400

    Redirect after POST, and show all items in the template

--- functional_tests.py
+++ functional_tests.py
@@ -58,12 +58,13 @@ class NewVisitorTest(unittest.TestCase):
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
+        self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
 
-        browser.quit()
+        self.browser.quit()
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
